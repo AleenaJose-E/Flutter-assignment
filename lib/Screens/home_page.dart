9e2d660b7926aa7299d0d8e89034faee5e588049
Alee-boop <84243631+Alee-boop@users.noreply.github.com>
2021-06-09 23:45:30 +0530

    add buttons

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -21,8 +21,7 @@ class  home_page extends StatelessWidget {
          CircleAvatar(radius: 60,backgroundImage: NetworkImage('https://images.unsplash.com/photo-1622295686469-ef9c9fa11be8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80'),),
         Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-       
-          children: [
+         children: [
             Container(
               height: 30,
             ),
@@ -30,10 +29,71 @@ class  home_page extends StatelessWidget {
             
             Text('A second Year computer Science student who want to learn and explore development and designing',
             style: TextStyle(fontSize: 15 ,backgroundColor: Colors.transparent ),)
-              ],
-        ),
-         ],
+             ],
+          
+           ),
+           Container(
+           height:25
+            ,
+           ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(padding: EdgeInsets.all(10)),
+              Container(
+                child: ElevatedButton(style:ElevatedButton.styleFrom(
+                  primary: Colors.transparent
+                ) , 
+                onPressed: () {  },
+                child: Text('About Me'
+              ) ,
+                ),
+              ),
+              Container(
+                width: 10,
+              ),
+              Container(
+                child: ElevatedButton(style:ElevatedButton.styleFrom(
+                  primary: Colors.transparent
+                ) , 
+                onPressed: () {  },
+                child: Text('Social media') ,
+             ),
+              ),
+              Container(
+              width: 10,
+              ),
+              Container(
+                child: ElevatedButton(style:ElevatedButton.styleFrom(
+                  primary: Colors.transparent
+                ) , 
+                onPressed: () {  },
+                child: Text('Hobbies') ,
+             ),
+              ),
+              
+            ],
+          ),  
+         Row(
+           
+           crossAxisAlignment: CrossAxisAlignment.center,
+           children: [
+             Padding(padding:EdgeInsets.all(10)),
+             Container(child: ElevatedButton(style:ElevatedButton.styleFrom(
+                  primary: Colors.transparent
+                ) , 
+                onPressed: () {  },
+                child: Text('Experience') ,
+                ),
+              ),
+           ],
+         ),
+          
+           
+       ] 
+         
       )),
+      
       
     );
   }
